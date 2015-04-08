b92df6d2a14a3c9ffdedbd2a7b02998836d394b5
Jan Stetina <jan.s@icanlocalize.com>
2015-04-08 14:07:07 +0200

    Do not try to install WordPress 3.4.1 with wp-cli, only download and extract the files.

--- www/wp341/vvv-init.sh
+++ www/wp341/vvv-init.sh
@@ -4,15 +4,15 @@ if [ ! -d "htdocs/wp-admin" ]; then
 		mkdir ./htdocs
 	fi
 	cd ./htdocs
+
+	# We can only download this version. wp-cli supports WordPress 3.5.2 and above.
 	wp core download --allow-root  --version=3.4.1
-	wp core config --dbname="wp341" --dbuser=wp --dbpass=wp --dbhost="localhost" --allow-root --extra-php <<PHP
-define('WP_DEBUG', true);
-define('WP_DEBUG_LOG', true);
-PHP
-	wp core install --url=wp341.wordpress.dev --title="wp341" --admin_user=admin --admin_password=password --admin_email=admin@localhost.dev --allow-root
-	
-	
-  
+
+	#wp core config --dbname="wp341" --dbuser=wp --dbpass=wp --dbhost="localhost" --allow-root --extra-php <<PHP
+#define('WP_DEBUG', true);
+#define('WP_DEBUG_LOG', true);
+#PHP
+	#wp core install --url=wp341.wordpress.dev --title="wp341" --admin_user=admin --admin_password=password --admin_email=admin@localhost.dev --allow-root
 	
 	cd -
 
