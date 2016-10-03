ad3441b8182f588891d66f2647e6a3b29dae4871
Jan Stetina <jan.s@icanlocalize.com>
2016-10-03 14:42:57 +0200

    Add extra git aliases

--- provision/provision.sh
+++ provision/provision.sh
@@ -238,6 +238,20 @@ configure_git() {
   if [[ $git_email ]]; then
 	noroot git config --global user.email "$git_email"
   fi
+  
+  # Extra aliases
+  #
+  # https://hackernoon.com/lesser-known-git-commands-151a1918a60
+  git config --global alias.please 'push --force-with-lease'
+  git config --global alias.commend 'commit --amend --no-edit'
+  git config --global alias.it '!git init && git commit -m “root” --allow-empty'
+  git config --global alias.stsh 'stash --keep-index'
+  git config --global alias.staash 'stash --include-untracked'
+  git config --global alias.staaash 'stash --all'
+  git config --global alias.shorty 'status --short --branch'
+  git config --global alias.merc 'merge --no-ff'
+  git config --global alias.grog 'log --graph --abbrev-commit --decorate --all --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)"'
+  
 }
 
 not_installed() {
